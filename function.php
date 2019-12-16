1177dec2af48ded026ab1ea3fcd2232d9e33f767
“michimoy” <yuji.it5668@gmail.com>
2019-12-16 14:16:19 +0900

    DB接続情報に誤りがあったため修正

--- function.php
+++ function.php
@@ -205,8 +205,8 @@ function dbconnect(){
   $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['dbname'] = ltrim($db['path'], '/');
   $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = 'root';
-  $password = 'root';
+  $user = $db['user'];
+  $password = $db['pass'];
   $options = array(
     // SQL実行失敗時にはエラーコードのみ設定
     PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
